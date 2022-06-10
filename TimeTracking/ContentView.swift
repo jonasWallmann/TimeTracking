a8f67f5ad31741365c7f145eb5818a3528163edc
Jonas Wallmann <jonas.wallmann@code.berlin>
2022-06-10 14:11:15 +0200

    Initial Commit

---
+++ TimeTracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeTracking
+//
+//  Created by Jonas Wallmann on 10.06.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeTracking/TimeTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackingApp.swift
+//  TimeTracking
+//
+//  Created by Jonas Wallmann on 10.06.22.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
